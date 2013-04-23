2b5c26ba40c2536e8c9cddf81807e78465f1bba6
Cody McCarthy <cody.mccarthy@colibri-software.com>
2013-04-23 12:24:54 -0300

    not working with crack function

--- zipcracker.c
+++ zipcracker.c
@@ -1,20 +1,32 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <time.h>
+#include <pthread.h>
+
+#define MAX_THREAD_NUM 50
+int count = 0;
+time_t start, stop;
+int diff;
+int ret;
+
+void * crack();
 int unzip(char * pass);
 int main(int argc, char * argv[])
 {
+    pthread_t thread[MAX_THREAD_NUM];
+    pthread_t thread1;
+    pthread_attr_t attr;
+    int id;
+    int* ids_t = (int *) malloc(MAX_THREAD_NUM * sizeof(int));
+    int i;
+    for (i = 0; i < MAX_THREAD_NUM; i++)
+        ids_t[i] = i;
+
     int min_char = 1;
     int max_char = 3;
-    int ret;
     char * start_pat = "a";
-    int i, j, k, l, m, n, o, p, q, r;
     char * end_pat = "zzz";
-    char alphanum[63] = 
-        "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-        "abcdefghijklmnopqrstuvwxyz"
-        "0123456789";
-
     if (argc == 5)
     {
         min_char = atoi(argv[1]);
@@ -22,31 +34,9 @@ int main(int argc, char * argv[])
         start_pat = argv[3];
         end_pat = argv[4];
     }
-    char string1[max_char + 1];
-    for (i = 0; i < strlen(alphanum); i++) {
-        string1[0] = alphanum[i];
-        for (j = 0; j < strlen(alphanum); j++) {
-            string1[1] = alphanum[j];
-            for (k = 0; k < strlen(alphanum); k++) {
-                string1[2] = alphanum[k];
-                system("rm -rf ggg");
-                ret = unzip(string1);
-                if (ret == 0)
-                {
-
-                    printf("PASS: %s", string1);
-                    break;
-                }
-            }
-            if (ret == 0)
-                break;
-
-        }
-
-        if (ret == 0)
-            break;
-    }
-    unzip(start_pat);
+    //pthread_create(&thread1,NULL, crack, (void *)&ids_t[0]);
+   // pthread_join(thread1, NULL);
+    crack();
     //  char  test = 'a';
     //  printf("test1: %s\n", test);
     //  test++;
@@ -55,13 +45,52 @@ int main(int argc, char * argv[])
     //  printf("Cmd: %s\n", cmd);
     return 0;
 }
+void * crack()
+{
+char alphanum[63] = 
+"ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+"abcdefghijklmnopqrstuvwxyz"
+"0123456789";
+
+
+    int i, j, k, l, m, n, o, p, q, r;
+    char string1[3 + 1];
+    start = time(NULL);
+    for (i = 0; i < strlen(alphanum); i++) {
+        string1[0] = alphanum[i];
+        for (j = 0; j < strlen(alphanum); j++) {
+            string1[1] = alphanum[j];
+            for (k = 0; k < strlen(alphanum); k++) {
+                string1[2] = alphanum[k];
+                system("rm -rf ggg");
+                ret = unzip(string1);
+                count++;
+                if (ret == 0)
+                {
+                    stop = time(NULL);
+                    diff = difftime(stop, start);
+                    printf("PASS: %s\n", string1);
+                    printf("Time: %d\n", diff);
+                    printf("count: %d\n", count);
+                    printf("PPS: %d\n", count / diff);
+                    break;
+                }
+            }
+            if (ret == 0)
+                break;
+        }
+        printf("String1: %s\n", string1);
+        if (ret == 0)
+            break;
+    }
+    return NULL;
+}
 int unzip(char * pass)
 {
     char cmd[80];
     strcpy(cmd, "unzip -qu -P ");
     strcat(cmd, pass);
-    strcat(cmd, " TestZip -d ./ggg");
+    strcat(cmd, " TestZip -d ./ggg > /dev/null 2>&1");
     int ret = system(cmd);
-
     return ret;
 }
